a18277b1fe2a3d3a99d9d63ec68788e97ef35e1c
Harishankar Banjare <harishankarlikeyou@gmail.com>
2025-03-05 16:38:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,6 +21,13 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+const corsOptions = {
+  origin: "https://image-upload-frontend-h8mr.onrender.com",
+  credentials: true,
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/api", imageRoutes);
